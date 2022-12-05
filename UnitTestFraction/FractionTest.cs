23045da82b3a6d1e4492cfb80f044724e7d25605
emk7online <emk7online@gmail.com>
2022-12-05 16:47:17 +0500

    Добавлен тествоый проект

---
+++ UnitTestFraction/FractionTest.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LibraryFraction;
+
+namespace UnitTestFraction
+{
+    [TestClass]
+    public class FractionTest
+    {
+        /// <summary>
+        /// должно вывести 1/10
+        /// </summary>
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //Arrange
+            string vxod = "1/10";
+            string expected = "1/10";
+            //Act
+            string actual = Fraction.FixWrongResult(vxod);
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+        /// <summary>
+        /// проверка на отсутствие букв
+        /// </summary>
+        [TestMethod]
+        public void NoLetter()
+        {
+            //Arrange
+            string vxod = "b/10";
+            //Act
+           Action actual = () => Fraction.FixWrongResult(vxod);
+            //Assert
+            Assert.ThrowsException<Exception>(actual);
+        }
+        /// <summary>
+        /// должна быть дробь
+        /// </summary>
+        [TestMethod]
+        public void FixWrongResult_MustBeenSlach_ReturnException()
+        {
+            //Arrange
+            string vxod = "1010";
+            //Act
+            Action actual = () => Fraction.FixWrongResult(vxod);
+            //Assert
+            Assert.ThrowsException<Exception>(actual);
+        }
+        /// <summary>
+        /// должно быть только 2 значения
+        /// </summary>
+        [TestMethod]
+        public void NoMoreTwo()
+        {
+            //Arrange
+            string vxod = "35/10/10/25";
+            //Act
+            Action actual = () => Fraction.FixWrongResult(vxod);
+            //Assert
+            Assert.ThrowsException<Exception>(actual);
+        }
+        /// <summary>
+        /// проверка на наличие нетолько слеша
+        /// </summary>
+        [TestMethod]
+        public void MustBeenNumber()
+        {
+            //Arrange
+            string vxod = "/";
+            //Act
+            Action actual = () => Fraction.FixWrongResult(vxod);
+            //Assert
+            Assert.ThrowsException<Exception>(actual);
+        }
+        /// <summary>
+        /// не пустое
+        /// </summary>
+        [TestMethod]
+        public void Notnull()
+        {
+            //Arrange
+            string vxod = "";
+            //Act
+            Action actual = () => Fraction.FixWrongResult(vxod);
+            //Assert
+            Assert.ThrowsException<Exception>(actual);
+        }
+    }
+}
---
+++ UnitTestFraction/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestFraction")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestFraction")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("49e7e618-d8a2-4da8-9032-33b9f1c84a31")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
